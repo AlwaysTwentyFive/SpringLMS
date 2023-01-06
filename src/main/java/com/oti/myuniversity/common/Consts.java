c912419d9324aaa78a6d3bc7615e8386a292a27f
ztrl <ztrl@naver.com>
2023-01-06 12:24:24 +0900

    MemberController updated, Consts Class implemented

---
+++ src/main/java/com/oti/myuniversity/common/Consts.java
@@ -0,0 +1,9 @@
+package com.oti.myuniversity.common;
+
+public final class Consts {
+	private Consts() {};
+	
+	public static final int ROWS_PER_PAGE = 10;
+	public static final int PAGES_PER_GROUP = 5;
+	public static final String DUMMY_PASSWORD = "!";
+}
