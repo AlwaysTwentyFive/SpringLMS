756d40c0d127289d7c6a313114fa32c36c2870bc
ztrl <ztrl@naver.com>
2023-01-06 09:16:16 +0900

    LoginInterceptor implemented

--- src/main/java/com/oti/myuniversity/common/LoginInterceptor.java
+++ src/main/java/com/oti/myuniversity/common/LoginInterceptor.java
@@ -2,17 +2,37 @@ package com.oti.myuniversity.common;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.oti.myuniversity.member.model.Member;
 
 public class LoginInterceptor implements HandlerInterceptor{
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		// TODO Auto-generated method stub
-		return false;
+		HttpSession session = request.getSession();
+		String contextName = request.getContextPath();
+		String url = request.getRequestURI().replaceFirst(contextName, "");
+		
+		String param = request.getQueryString();
+		
+		if (!url.contains("member/login") && !url.contains("member/logout")) {
+			session.setAttribute("url", url);
+			session.setAttribute("param", param);
+		}
+		
+		Member member = (Member) session.getAttribute("member");
+		String memberId = member.getMemberId();
+		if (memberId != null || "".equals(memberId)) {
+			response.sendRedirect(request.getContextPath() + "/member/login");
+			return false;
+		}
+		
+		return true;
 	}
 
 	@Override
